5e6004b78091d018688e8eadf63433c640aa653a
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-04-30 22:31:37 +0800

    feat: 新增启停执行方法 & 新增 Logger & Config 类调用示例

--- PluginEntry.cs
+++ PluginEntry.cs
@@ -1,15 +1,25 @@
-using MSLX.SDK;
+﻿using MSLX.SDK;
 
 namespace MSLX.Plugin.Demo;
 
 public class PluginEntry : IPlugin
 {
     public string Id => "mslx-plugin-demo"; 
-    public string Name => "MSLX 官方示例插件";
-    public string Description => "MSLX 官方示例插件";
+    public string Name => "MSLX 示例插件";
+    public string Description => "MSLX 示例插件";
     public string Version => "1.0.1";
     public string MinLoaderVersion => "1.3.8";
     public string Developer => "xiaoyu";
     public string AuthorUrl => "https://github.com/MSLTeam";
     public string PluginUrl => "https://github.com/MSLTeam/mslx-plugin-demo";
+
+    public void OnLoad()
+    {
+        SDK.MSLX.Logger.Info("mslx-plugin-demo 载入成功~");
+        SDK.MSLX.Logger.Info("当前存在实例数量：" + SDK.MSLX.Config.Servers.GetServerList().Count.ToString());
+    }
+
+    public void OnUnload() {
+        SDK.MSLX.Logger.Info("mslx-plugin-demo 卸载成功~");
+    }
 }
