1aed69dd08ec3e844c48fdb563148b0a617f9fc5
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-05-13 16:31:24 +0800

    feat: 更新SDK到v1.4.3 dev版本 & 新增配置文件调用示例

--- MSLXPluginEntry.cs
+++ MSLXPluginEntry.cs
@@ -18,6 +18,19 @@ public class MSLXPluginEntry : IPlugin
     {
         SDK.MSLX.Logger.Info("mslx-plugin-demo 载入成功~");
         SDK.MSLX.Logger.Info("当前存在实例数量：" + SDK.MSLX.Config.Servers.GetServerList().Count.ToString());
+        
+        // ===== 配置读写示例 =====
+        string dataDir = this.Config().GetDataPath();
+        SDK.MSLX.Logger.Info("使用的数据目录："+dataDir);
+        
+        this.Config().WriteConfigKey("author", "xiaoyu");
+        this.Config().WriteConfigKey("magicNumber", 1027);
+        
+        int count = (int?)this.Config().ReadConfigKey("magicNumber") ?? 0;
+        SDK.MSLX.Logger.Info("从配置文件读取magicNumber：" + count.ToString());
+        
+        var allConfig = this.Config().ReadConfig();
+        
     }
 
     public void OnUnload() {
